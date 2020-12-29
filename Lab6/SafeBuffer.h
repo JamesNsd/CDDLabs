732485f7799eec9c7585a36f383bf998d9a59c8f
JamesNsd <55279169+JamesNsd@users.noreply.github.com>
2020-12-29 13:16:58 +0000

    Lab 6

---
+++ Lab6/SafeBuffer.h
@@ -0,0 +1,72 @@
+/* SafeBuffer.h --- 
+ * 
+ * Filename: SafeBuffer.h
+ * Description: 
+ * Author: Joseph
+ * Maintainer: 
+ * Created: Tue Jan  8 12:30:23 2019 (+0000)
+ * Version: 
+ * Package-Requires: ()
+ * Last-Updated: Tue Jan  8 12:30:25 2019 (+0000)
+ *           By: Joseph
+ *     Update #: 1
+ * URL: 
+ * Doc URL: 
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change Log:
+ * 
+ * 
+ */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Code: */
+
+/* Code: */
+
+class Buffer{
+  int front;
+  int back;
+  int capacity;
+  int count; //current size
+  int arr[10];
+
+ public:
+  Buffer(){
+   front =0;
+   back =-1;
+   count = 0;
+   capacity = 10;
+  }
+  int size();
+  bool isEmpty();
+  bool isFull();
+  void enqueue(int value);
+  void dequeue();  
+  int viewBack();
+  int viewFront(); 
+};
+
+/* SafeBuffer.h ends here */
---
+++ Lab6/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['buffer_2',['Buffer',['../classBuffer.html',1,'']]]
+];
